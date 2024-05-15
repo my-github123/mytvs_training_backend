fc67d2697c22fa8d10c677231047dcfa79658edb
yuvashree27 <yuvashree.s@gobumpr.com>
2024-05-15 11:59:26 +0530

    user model changes

--- src/model/User.js
+++ src/model/User.js
@@ -2,7 +2,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const User = sequelize.define("user", {
+const User = sequelize.define("users", {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
